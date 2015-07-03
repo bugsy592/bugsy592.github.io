71a2340bcb8a6e929bb2de51e77bc887d9dd4118
Nick Ballback <nickballback@gmail.com>
2015-07-03 16:52:06 -0400

    Setup Spigot

---
+++ Spigot/src/main/java/me/bugsy592/TestPluginSpigot/TestPlugin.java
@@ -0,0 +1,15 @@
+package me.bugsy592.TestPluginSpigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin{
+	 // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
